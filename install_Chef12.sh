5679b6e7e618bdb0da95e311aa5b8852160e17ab
Vlad Turbuleasa-Jurje <vturbuleasa@endava.net>
2016-02-11 10:03:04 +0200

    Remove install_Chef12.sh

--- install_Chef12.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -s https://packagecloud.io/install/repositories/chef/stable/script.rpm.sh | sudo bash
-yum install chef-server-core
-chef-server-ctl reconfigure
-mkdir -p /opt/chef-server
-sudo chef-server-ctl user-create --filename /opt/chef-server/vagrant.pem vagrant Vlad ENDAVA vlad.turbuleasa@endava.com vagrant
-sudo chef-server-ctl org-create ssh DevOps --association_user vagrant --filename /opt/chef-server/ssh.pem
